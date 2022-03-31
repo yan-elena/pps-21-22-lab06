7f6a75525518770c707faae5aebe70b56eacdbea
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-03-31 23:21:41 +0200

    prepare for scala 3

--- src/test/scala/u06lab/code/Parser.scala
+++ src/test/scala/u06lab/code/Parser.scala
@@ -1,9 +1,9 @@
 package u06lab.code
 
-import org.junit.jupiter.api.{Assertions, Test}
-import Assertions._
+import org.junit.Test
+import org.junit.Assert.*
 
-class ParserTests {
+class ParserTests:
   def parser = new BasicParser(Set('a', 'b', 'c'))
   // Note NonEmpty being "stacked" on to a concrete class
   // Bottom-up decorations: NonEmptyParser -> NonEmpty -> BasicParser -> Parser
@@ -14,39 +14,31 @@ class ParserTests {
   def sparser: Parser[Char] = ??? // "abc".charParser()
 
   @Test
-  def testBasicParser = {
+  def testBasicParser =
     assertTrue(parser.parseAll("aabc".toList))
     assertFalse(parser.parseAll("aabcdc".toList))
     assertTrue(parser.parseAll("".toList))
-  }
 
   @Test
-  def testNotEmptyParser = {
+  def testNotEmptyParser =
     assertTrue(parserNE.parseAll("0101".toList))
     assertFalse(parserNE.parseAll("0123".toList))
     assertFalse(parserNE.parseAll(List()))
-  }
 
   @Test
-  def testNotTwoConsecutiveParser = {
+  def testNotTwoConsecutiveParser =
     assertTrue(parserNTC.parseAll("XYZ".toList))
     assertFalse(parserNTC.parseAll("XYYZ".toList))
     assertTrue(parserNTC.parseAll("".toList))
-  }
 
   @Test
-  def testNotEmptyAndNotTwoConsecutiveParser = {
+  def testNotEmptyAndNotTwoConsecutiveParser =
     assertTrue(parserNTCNE.parseAll("XYZ".toList))
     assertFalse(parserNTCNE.parseAll("XYYZ".toList))
     assertFalse(parserNTCNE.parseAll("".toList))
-  }
 
   @Test
-  def testStringParser = {
+  def testStringParser =
     assertTrue(sparser.parseAll("aabc".toList))
     assertFalse(sparser.parseAll("aabcdc".toList))
     assertTrue(sparser.parseAll("".toList))
-  }
-}
-
-
